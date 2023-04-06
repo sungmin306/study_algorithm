2ee589ede9bca37dbe39c2fa41e2047fed95e23e
sungmin306 <another0306@naver.com>
2023-04-06 18:40:31 +0900

    4월 6일 잘못된 컨트리뷰터 삭제

---
+++ 인프런코딩테스트준비/재귀함수를 이용한 이진수 출력.py
@@ -0,0 +1,19 @@
+
+def binary_number(n,L):
+    if n == 0:
+        return L
+    d = n // 2
+    input_num = n % 2
+    L.append(input_num)
+    print(L)
+    return binary_number(d,L)
+
+n = int(input())
+L=[]
+B=[]
+B=binary_number(n,L)
+print(len(B))
+
+for i in range(len(B)-1,-1,-1):
+    print(B[i],end="")
+print("") # 1
