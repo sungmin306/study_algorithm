2107666650c887844e75d190a8eba45ecb089d93
sungmin306 <another0306@naver.com>
2023-04-27 21:37:52 +0900

    4월 27일 재귀함수 이진수 맥

--- 인프런코딩테스트준비/재귀함수를 이용한 이진수 출력.py
+++ 인프런코딩테스트준비/재귀함수를 이용한 이진수 출력.py
@@ -5,15 +5,15 @@ def binary_number(n,L):
     d = n // 2
     input_num = n % 2
     L.append(input_num)
-    print(L)
+    #print(L)
     return binary_number(d,L)
 
 n = int(input())
 L=[]
 B=[]
 B=binary_number(n,L)
-print(len(B))
+#print(len(B))
 
 for i in range(len(B)-1,-1,-1):
     print(B[i],end="")
-print("") # 1
+print("") 
