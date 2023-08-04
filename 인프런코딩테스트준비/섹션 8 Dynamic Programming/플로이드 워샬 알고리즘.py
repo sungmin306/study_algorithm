1449b5630ca7a522cfc28455e5ac39049783ad05
sungmin306 <another0306@naver.com>
2023-08-04 23:36:17 +0900

    DP 13번까지 맥

---
+++ 인프런코딩테스트준비/섹션 8 Dynamic Programming/플로이드 워샬 알고리즘.py
@@ -0,0 +1,20 @@
+n , m = map(int,input().split())
+DP = [[10000] * (n+1) for _ in range(n+1)]
+for i in range(n+1):
+    DP[i][i] = 0
+for _ in range(m):
+    i , j , d = map(int,input().split())
+    DP[i][j] = d
+    
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            DP[i][j] = min(DP[i][j], DP[i][k] + DP[k][j])
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if DP[i][j] == 10000:
+            print("M",end=' ')
+        else:
+            print(DP[i][j],end=' ')
+    print()
