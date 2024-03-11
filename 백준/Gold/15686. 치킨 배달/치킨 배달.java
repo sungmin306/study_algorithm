bebb931ac98246af1859ac16e7d2f9e0c95609ba
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-03-11 17:32:51 +0900

    [Gold V] Title: 치킨 배달, Time: 240 ms, Memory: 18160 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -0,0 +1,67 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int n;
+    public static int m;
+    public static int result;
+    public static int[][] map;
+    public static boolean[] open;
+
+    public static ArrayList<int[]> house;
+    public static ArrayList<int[]> chicken;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][n];
+        house = new ArrayList<>();
+        chicken = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j  = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+                if(map[i][j] == 1) house.add(new int[] {i,j});
+                else if(map[i][j] == 2) chicken.add(new int[] {i,j});
+            }
+        }
+        result = Integer.MAX_VALUE;
+        open = new boolean[chicken.size()];
+        dfs(0,0);
+        System.out.println(result);
+    }
+    public static void dfs(int start, int cnt) {
+        if(cnt == m) {
+            int res = 0;
+            for(int i = 0; i < house.size(); i++) {
+                int temp = Integer.MAX_VALUE;
+                for(int j = 0; j < chicken.size(); j++) {
+                    if(open[j]) {
+                        int distance = Math.abs(house.get(i)[0] - chicken.get(j)[0])
+                                + Math.abs(house.get(i)[1] - chicken.get(j)[1]);
+                        temp = Math.min(temp, distance);
+                    }
+                }
+                res += temp;
+            }
+            result = Math.min(result, res);
+            return;
+
+        }
+
+        for(int i = start; i < chicken.size(); i++) {
+            open[i] = true;
+            dfs(i+ 1, cnt + 1);
+            open[i] = false;
+        }
+    }
+}
