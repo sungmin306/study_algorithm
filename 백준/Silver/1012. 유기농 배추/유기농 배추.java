62c98ef8e2c2bf7c71284e816e084c89245469b4
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-05-02 13:05:48 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,66 +1,75 @@
+
 import java.util.*;
 import java.io.*;
-
 public class Main {
     public static int T;
-    public static int n,m,k;
+    public static int N, M, K;
+
+    public static int X,Y;
+
+    public static int result;
+
     public static int[][] map;
     public static boolean[][] visited;
-    public static StringBuilder sb;
-    public static int[] dx = {1,-1,0,0};
-    public static int[] dy = {0,0,-1,1};
+
+    public static int[] dx = {-1,0,1,0};
+    public static int[] dy = {0,1,0,-1};
+
+    public static boolean inRange(int x, int y) {
+        return 0<= x && x < N && 0 <= y && y < M;
+    }
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for(int i = 0; i  < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(inRange(nx,ny)) {
+                if(!visited[nx][ny] && map[nx][ny] == 1) {
+                    dfs(nx,ny);
+                }
+            }
 
 
+        }
+    }
 
     public static void main(String[] args)throws IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
-        T = Integer.parseInt(br.readLine());
-        for(int i = 0; i < T; i++) {
+        T = Integer.parseInt(br.readLine()); // 테스트 케이스
+
+        for(int t = 0; t < T; t++) {
+
             StringTokenizer st = new StringTokenizer(br.readLine());
-            m = Integer.parseInt(st.nextToken());
-            n = Integer.parseInt(st.nextToken());
-            k = Integer.parseInt(st.nextToken());
-            map = new int[n][m];
-            visited = new boolean[n][m];
-            for(int j = 0; j < k; j++) {
+            M = Integer.parseInt(st.nextToken()); // 가로
+            N = Integer.parseInt(st.nextToken()); // 세로
+            K = Integer.parseInt(st.nextToken());
+            map = new int[N][M];
+            visited = new boolean[N][M];
+            result = 0;
+            for(int k = 0; k < K; k++) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                map[y][x]= 1;
+                X = Integer.parseInt(st.nextToken());
+                Y = Integer.parseInt(st.nextToken());
+                map[Y][X] = 1;
             }
-            bfs();
-        }
-        System.out.println(sb);
-    }
-    public static void bfs() {
-        int cnt = 0;
-        for(int x = 0; x < map.length; x++) {
-            for(int y = 0; y < map[0].length; y++) {
-                if (visited[x][y] || map[x][y] == 0)
-                    continue;
-                Queue<int []> queue = new LinkedList<>();
-                queue.add(new int[] {x,y});
 
-                while(!queue.isEmpty()) {
-                    int[] now = queue.poll();
-                    int nowX = now[0];
-                    int nowY = now[1];
-
-                    for(int i = 0; i < 4; i++) {
-                        int newX = nowX + dx[i];
-                        int newY = nowY + dy[i];
-                        if (newX < 0 || newY < 0 || newX >= n || newY >= m)
-                            continue;
-                        if (visited[newX][newY] || map[newX][newY] == 0)
-                            continue;
-                        queue.add(new int[] {newX,newY});
-                        visited[newX][newY] = true;
+            for(int i = 0; i < map.length; i++) {
+                for(int j = 0; j < map[0].length; j++) {
+                    if(!visited[i][j] && map[i][j] == 1) {
+                        dfs(i,j);
+                        result++;
                     }
                 }
-                cnt++;
             }
+            sb.append(result).append("\n");
         }
-        sb.append(cnt).append("\n");
+        System.out.println(sb);
+
+
     }
+
+
 }
