c7bf1deb7716745ee1c3e86457630df7ebfbe9af
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-06-12 16:58:22 +0900

    [Silver III] Title: N과 M (4), Time: 168 ms, Memory: 19000 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,4 +1,5 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,30 +8,21 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int n;
-    public static int m;
-
+    public static int N, M;
+    public static ArrayList<Integer> arrayList = new ArrayList<>();
     public static StringBuilder sb = new StringBuilder();
 
-    public static ArrayList<Integer> arrayList = new ArrayList<>();
-
-    public static void print() {
-        for(int i = 0; i < arrayList.size(); i++) {
-            sb.append(arrayList.get(i)).append(" ");
-        }
-    }
-
-    public static void dfs(int h, int v) {
-
-        if(h == m + 1) {
-            print();
+    public static void dfs(int d,int n) {
+        if(d == M) {
+            for(int i = 0; i < arrayList.size(); i++) {
+                sb.append(arrayList.get(i)).append(" ");
+            }
             sb.append("\n");
             return;
         }
-
-        for(int i = v; i <= n; i++) {
+        for(int i = n; i <= N; i++) {
             arrayList.add(i);
-            dfs(h + 1,i);
+            dfs(d + 1,i);
             arrayList.remove(arrayList.size() - 1);
         }
     }
@@ -38,12 +30,12 @@ public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
 
-        dfs(1,1);
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dfs(0,1);
 
         System.out.println(sb);
-
     }
 }
