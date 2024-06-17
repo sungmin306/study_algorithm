408c83a4d2df3b616faca39202c57a0ccf6f0e88
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-06-17 20:20:43 +0900

    [Silver III] Title: N과 M (3), Time: 556 ms, Memory: 214136 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,43 +1,39 @@
 
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static int n, m;
+    public static int N, M;
     public static ArrayList<Integer> arrayList = new ArrayList<>();
     public static StringBuilder sb = new StringBuilder();
-
-    public static void print() {
-        for(int i = 0; i < arrayList.size(); i++) {
-            sb.append(arrayList.get(i)).append(" ");
-        }
-        sb.append("\n");
-    }
-    public static void dfs(int h) {
-        if(h == m + 1) {
-            print();
-            return;
-        }
-        for(int i = 1; i <= n; i++) {
-            arrayList.add(i);
-            dfs(h + 1);
-            arrayList.remove(arrayList.size() - 1);
-        }
-
-    }
-
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        dfs(1);
-
+        dfs(0);
         System.out.println(sb);
+    }
 
+    private static void dfs(int d) {
+        if(d == M) {
+            for(int i = 0; i < arrayList.size(); i++) {
+                sb.append(arrayList.get(i)).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 1; i <= N; i++) {
+            arrayList.add(i);
+            dfs(d + 1);
+            arrayList.remove(arrayList.size() - 1);
+        }
     }
 }
