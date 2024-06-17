fddf5647471c9e866e979b4ca9c1f89d057e0382
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-06-17 20:07:35 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,41 +1,41 @@
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int n, m;
-
+    public static int N, M;
     public static ArrayList<Integer> arrayList = new ArrayList<>();
+    public static StringBuilder sb = new StringBuilder();
 
-
-    public static void print() {
-        for(int i = 0; i < arrayList.size(); i++) {
-            System.out.print(arrayList.get(i) + " ");
-        }
-        System.out.println();
-    }
-
-    public static void dfs(int v, int h) {
-        if(h == m + 1) {
-            print();
+    public static void dfs(int d, int n) {
+        if(d == M) {
+            for (int i = 0; i < arrayList.size(); i++) {
+                sb.append(arrayList.get(i)).append(" ");
+            }
+            sb.append("\n");
             return;
         }
-        for(int i = v; i <= n; i++) {
+        for(int i = n; i <= N; i++) {
             arrayList.add(i);
-            dfs(i + 1, h + 1);
+            dfs(d + 1, i + 1);
             arrayList.remove(arrayList.size() - 1);
-
         }
     }
 
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        dfs(0,1);
 
-        dfs(1,1);
+        System.out.println(sb);
     }
+
+
 }
