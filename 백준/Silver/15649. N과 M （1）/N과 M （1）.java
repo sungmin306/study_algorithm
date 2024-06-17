9df69ddb4171759127ff47027c453e7ce5f53b93
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-06-17 19:54:01 +0900

    [Silver III] Title: N과 M (1), Time: 304 ms, Memory: 38416 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,44 +8,39 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int n, m;
+    public static int N, M;
     public static ArrayList<Integer> arrayList = new ArrayList<>();
+    public static StringBuilder sb = new StringBuilder();
     public static boolean[] visited;
-
-    public static void print() {
-        for(int i = 0; i < arrayList.size(); i++) {
-            System.out.print(arrayList.get(i) + " ");
-        }
-        System.out.println();
-    }
-    public static void dfs(int h) {
-        if(h == m + 1) {
-            print();
+    public static void dfs(int d) {
+        if(d == M) {
+            for(int i = 0; i < arrayList.size(); i++) {
+                sb.append(arrayList.get(i)).append(" ");
+            }
+            sb.append("\n");
             return;
         }
-        for(int i = 1; i <= n; i++) {
-            if(visited[i]) continue;
 
+        for(int i = 1; i <= N; i++) {
+
+            if(visited[i]) continue;
             arrayList.add(i);
             visited[i] = true;
-            dfs(h + 1);
-            arrayList.remove(arrayList.size() - 1);
+            dfs(d + 1);
             visited[i] = false;
+            arrayList.remove(arrayList.size() - 1);
         }
 
     }
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st  = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        dfs(0);
 
-        visited = new boolean[n + 1];
-
-        dfs(1);
-
-
-
+        System.out.println(sb);
     }
 }
