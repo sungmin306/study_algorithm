126112e07286c6508dd519e11f3a6a399f149a86
조성민 <101984130+sungmin306@users.noreply.github.com>
2025-04-07 23:41:10 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
+FROM
+    ANIMAL_INS
+WHERE
+    NAME = 'Ella'
+    OR NAME = 'LUCY'
+    OR NAME = 'Pickle'
+    OR NAME = 'Rogan'
+    OR NAME = 'Sabrina'
+    OR NAME= 'Mitty';
