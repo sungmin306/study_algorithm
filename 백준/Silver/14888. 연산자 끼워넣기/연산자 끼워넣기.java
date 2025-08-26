ea4f85e2dba044e69cda4364a0835cea89779a70
조성민 <101984130+sungmin306@users.noreply.github.com>
2025-08-26 22:29:11 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 116 ms, Memory: 15664 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int N;
+    public static int[] number;
+    public static int[] operator = new int[4];
+    public static int MAX = -1000000000;
+    public static int MIN = 1000000000;
+
+    public static void dfs(int num, int index) {
+        if(index == N) {
+            MAX = Math.max(MAX, num);
+            MIN = Math.min(MIN, num);
+        }
+
+        for(int i = 0; i < 4; i++) {
+            if(operator[i] > 0) {
+                operator[i]--;
+                switch(i) {
+                    case 0: dfs(num + number[index], index+1); break;
+                    case 1: dfs(num - number[index], index+1); break;
+                    case 2: dfs(num * number[index], index+1); break;
+                    case 3: dfs(num / number[index], index+1); break;
+                }
+                operator[i]++;
+            }
+        }
+    }
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        number = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 4; i++) {
+            operator[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(number[0],1);
+
+        System.out.println(MAX);
+        System.out.println(MIN);
+
+    }
+}
