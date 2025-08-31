c819c8658c17a3406101382182bd44220203c4f2
조성민 <101984130+sungmin306@users.noreply.github.com>
2025-08-31 18:34:29 +0900

    [Bronze II] Title: 소수, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -7,32 +7,30 @@ public class Main {
     public static boolean isPrime(int a) {
         if(a < 2) return false;
         for(int i = 2; i <= a/2; i++) {
-            if(a % i == 0) {
-                return false;
-            }
+            if(a % i == 0) return false;
         }
         return true;
     }
 
+
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int M = Integer.parseInt(br.readLine());
         int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int min = Integer.MAX_VALUE;
         int sum = 0;
-        int min = 0;
-        boolean check = false;
-        for(int i = M; i <=N; i++) {
+        for(int i = N; i <= M; i++) {
             if(isPrime(i)) {
-                check = true;
-                if(min == 0) min = i;
-                sum+= i;
+                if(min > i) min = i;
+                sum += i;
             }
         }
-        if (check) {
+        if(min == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        }
+        else {
             System.out.println(sum);
             System.out.println(min);
-        } else {
-            System.out.println(-1);
         }
     }
 }
