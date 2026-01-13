f802bb4943bce32fe846dc94303f04f40cca6b16
조성민 <101984130+sungmin306@users.noreply.github.com>
2026-01-13 10:10:02 +0900

    [Gold IV] Title: 테트로미노, Time: 584 ms, Memory: 36508 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,4 +1,5 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,17 +9,15 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int N,M;
-    public static int ans = 0;
-
+    public static int N, M;
     public static int[][] map;
     public static boolean[][] visited;
-
-    public static int[] dx = {-1,0,1,0};
+    public static int ans;
+    public static int[] dx = {1,0,-1,0};
     public static int[] dy = {0,1,0,-1};
 
-    public static boolean inRange(int x,int y) {
-            return 0 <= x && x < N && 0 <= y && y < M;
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < M;
     }
 
     public static void dfs(int x, int y, int depth, int sum) {
@@ -34,36 +33,37 @@ public class Main {
             if(!inRange(nx,ny) || visited[nx][ny]) continue;
 
             visited[nx][ny] = true;
-            dfs(nx,ny,depth +1, sum + map[nx][ny]);
+            dfs(nx,ny,depth+1, sum + map[nx][ny]);
             visited[nx][ny] = false;
         }
     }
 
     public static void checkT(int x, int y) {
         int center = map[x][y];
-
         int sum = center;
-        int minArm = Integer.MAX_VALUE;
         int armCount = 0;
+        int minArm = Integer.MAX_VALUE;
 
         for(int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (!inRange(nx,ny)) continue;
+
+            if(!inRange(nx,ny)) continue;
 
             armCount++;
             int v = map[nx][ny];
-            sum += v;
             minArm = Math.min(minArm, v);
+            sum += v;
         }
 
-        if (armCount < 3) return;
+        if(armCount < 3) return;
 
-        if (armCount == 3) {
+        if(armCount == 3) {
             ans = Math.max(ans, sum);
         } else {
-            ans = Math.max(ans, sum - minArm);
+            ans = Math.max(ans , sum - minArm);
         }
+
     }
 
 
@@ -73,7 +73,6 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         map = new int[N][M];
         visited = new boolean[N][M];
 
@@ -84,12 +83,13 @@ public class Main {
             }
         }
 
+        ans = 0;
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < M; j++) {
                 visited[i][j] = true;
-                dfs(i,j,1,map[i][j]);
-                visited[i][j] = false;
+                dfs(i,j,1, map[i][j]);
                 checkT(i,j);
+                visited[i][j] = false;
             }
         }
         System.out.println(ans);
