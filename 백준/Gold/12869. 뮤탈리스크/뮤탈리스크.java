64cc6ca66bb0f712b63767239e9eeef372daba9f
조성민 <101984130+sungmin306@users.noreply.github.com>
2026-01-18 15:57:16 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 112 ms, Memory: 16968 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -2,17 +2,23 @@
 
 import java.util.*;
 import java.io.*;
+
 public class Main {
 
     public static int[][] damage = {{-9,-3,-1},{-9,-1,-3},{-3,-9,-1},{-3,-1,-9},{-1,-3,-9},{-1,-9,-3}};
     static int[][][] dp;
     static int min;
+
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int [] scv = new int[3];
-        for(int i = 0; i < n; i++) scv[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++) {
+            scv[i] = Integer.parseInt(st.nextToken());
+        }
+
         dp = new int[61][61][61];
         min = Integer.MAX_VALUE;
         dfs(scv,0);
@@ -23,13 +29,20 @@ public class Main {
         int scv2 = scv[1];
         int scv3 = scv[2];
 
-        if(min <= cnt) return;
-        if(dp[scv1][scv2][scv3] != 0 && dp[scv1][scv2][scv3] <= cnt) return;
+        if(min <= cnt){
+            return;
+        }
+
+        if(dp[scv1][scv2][scv3] != 0 && dp[scv1][scv2][scv3] <= cnt) {
+            return;
+        }
+
         dp[scv1][scv2][scv3] = cnt;
         if(scv1 == 0 && scv2 == 0 && scv3 ==0) {
             min = Math.min(cnt,min);
             return;
         }
+
         for(int i = 0; i < 6; i++) {
             int newScv1 = Math.max(scv1 + damage[i][0], 0);
             int newScv2 = Math.max(scv2 + damage[i][1], 0);
@@ -39,5 +52,4 @@ public class Main {
         }
 
     }
-
 }
