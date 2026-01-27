a8f2e4c6d067609841f971d7e0dc7344cbb72b7a
조성민 <101984130+sungmin306@users.noreply.github.com>
2026-01-27 16:00:19 +0900

    [Silver II] Title: 외판원 순회 2, Time: 104 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -5,39 +5,31 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int N, ans;
-    public static int[] numbers;
+    public static int N,ans;
     public static int[][] W;
     public static boolean[] visit;
 
-    public static void dfs(int d, int start, int prev, int sum) {
-        if(sum >= ans) return; // 백트래킹
+    public static void dfs(int depth, int start, int prev, int sum) {
+        if(sum >= ans) return;
 
-        if(d == N) {
-            if(W[prev][start] == 0) return; // 돌아오는 길 없음
-            ans = Math.min(ans,sum + W[prev][start]);
+        if(depth == N) {
+            if(W[prev][start] == 0) return;
+            ans = Math.min(ans, sum + W[prev][start]);
             return;
         }
 
-        for(int next = 0; next < N; next++) { {
+        for(int next = 0; next < N; next++) {
             if(visit[next]) continue;
             if(W[prev][next] == 0) continue;
-
             visit[next] = true;
-            dfs(d + 1, start, next, sum + W[prev][next]);
+            dfs(depth + 1, start, next, sum + W[prev][next]);
             visit[next] = false;
         }
-
-        }
-
-
     }
-
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         W = new int[N][N];
-        numbers = new int[N];
         visit = new boolean[N];
         ans = Integer.MAX_VALUE;
         for(int i = 0; i < N; i++) {
