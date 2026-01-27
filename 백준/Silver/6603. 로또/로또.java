fd9285c1ee92d08fa23d09d01d5e05eddb893585
조성민 <101984130+sungmin306@users.noreply.github.com>
2026-01-27 16:50:14 +0900

    [Silver II] Title: 로또, Time: 108 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int N;
+    public static int[] arr;
+    public static int[] numbers;
+    public static StringBuilder sb;
+
+    public static void dfs(int idx, int depth) {
+        if(depth == 6) {
+            for(int i = 0; i < 6; i++) {
+                sb.append(numbers[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = idx + 1; i < N; i++) {
+            numbers[depth] = arr[i];
+            dfs(i,depth + 1);
+        }
+    }
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            sb = new StringBuilder();
+            N = Integer.parseInt(st.nextToken());
+            arr = new int[N];
+            numbers = new int[6];
+            if(N == 0) break;
+            for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+            Arrays.sort(arr);
+            for(int i = 0; i < N; i++) {
+                numbers[0] = arr[i];
+                dfs(i,1);
+            }
+            System.out.println(sb);
+            sb.append("\n");
+
+        }
+    }
+}
